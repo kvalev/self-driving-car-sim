a8ed02dcee99f03b52d87a571b5146a9995f1b0e
Krassimir Valev <krassimir_valev@yahoo.com>
2017-02-09 23:59:34 +0100

    Added a flag whether the car is on the track

--- Assets/1_SelfDrivingCar/Scripts/CommandServer.cs
+++ Assets/1_SelfDrivingCar/Scripts/CommandServer.cs
@@ -13,13 +13,15 @@ public class CommandServer : MonoBehaviour
 	private SocketIOComponent _socket;
 	private CarController _carController;
 
+	private const string TRACK_TAG = "Track";
+
 	// Use this for initialization
 	void Start()
 	{
 		_socket = GameObject.Find("SocketIO").GetComponent<SocketIOComponent>();
 		_socket.On("open", OnOpen);
 		_socket.On("steer", OnSteer);
-		_socket.On("manual", onManual);
+		_socket.On("manual", OnManual);
 		_carController = CarRemoteControl.GetComponent<CarController>();
 	}
 
@@ -35,15 +37,14 @@ public class CommandServer : MonoBehaviour
 	}
 
 	// 
-	void onManual(SocketIOEvent obj)
+	void OnManual(SocketIOEvent obj)
 	{
-		EmitTelemetry (obj);
+		EmitTelemetry(obj);
 	}
 
 	void OnSteer(SocketIOEvent obj)
 	{
 		JSONObject jsonObject = obj.data;
-		//    print(float.Parse(jsonObject.GetField("steering_angle").str));
 		CarRemoteControl.SteeringAngle = float.Parse(jsonObject.GetField("steering_angle").str);
 		CarRemoteControl.Acceleration = float.Parse(jsonObject.GetField("throttle").str);
 		EmitTelemetry(obj);
@@ -53,42 +54,28 @@ public class CommandServer : MonoBehaviour
 	{
 		UnityMainThreadDispatcher.Instance().Enqueue(() =>
 		{
+			Vector3 carPosition = _carController.transform.position;
+			Ray ray = new Ray(carPosition, Vector3.down);
+
+			RaycastHit hitInfo;
+			bool hit = Physics.Raycast(ray, out hitInfo);
+			bool onTrack = hit && hitInfo.collider.tag == TRACK_TAG;
+
 			print("Attempting to Send...");
+			Dictionary<string, string> data = new Dictionary<string, string>();
 			// send only if it's not being manually driven
-			if ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.S))) {
-				_socket.Emit("telemetry", new JSONObject());
-			}
-			else {
+			// duplicates the logic in UISystem
+			if (!Input.GetKey(KeyCode.W) || !Input.GetKey(KeyCode.S)) {
 				// Collect Data from the Car
-				Dictionary<string, string> data = new Dictionary<string, string>();
 				data["steering_angle"] = _carController.CurrentSteerAngle.ToString("N4");
 				data["throttle"] = _carController.AccelInput.ToString("N4");
 				data["speed"] = _carController.CurrentSpeed.ToString("N4");
 				data["image"] = Convert.ToBase64String(CameraHelper.CaptureFrame(FrontFacingCamera));
-				_socket.Emit("telemetry", new JSONObject(data));
-			}
-		});
 
-		//    UnityMainThreadDispatcher.Instance().Enqueue(() =>
-		//    {
-		//      	
-		//      
-		//
-		//		// send only if it's not being manually driven
-		//		if ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.S))) {
-		//			_socket.Emit("telemetry", new JSONObject());
-		//		}
-		//		else {
-		//			// Collect Data from the Car
-		//			Dictionary<string, string> data = new Dictionary<string, string>();
-		//			data["steering_angle"] = _carController.CurrentSteerAngle.ToString("N4");
-		//			data["throttle"] = _carController.AccelInput.ToString("N4");
-		//			data["speed"] = _carController.CurrentSpeed.ToString("N4");
-		//			data["image"] = Convert.ToBase64String(CameraHelper.CaptureFrame(FrontFacingCamera));
-		//			_socket.Emit("telemetry", new JSONObject(data));
-		//		}
-		//      
-		////      
-		//    });
+				data["ontrack"] = onTrack.ToString();
+			}
+
+			_socket.Emit("telemetry", new JSONObject(data));
+		});
 	}
 }
